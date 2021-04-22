db30d6359a3af81b5e0ccde893d14beba93a3ee8
juancedeno96 <cedenojj7@gmail.com>
2021-04-22 13:26:36 -0600

    Set up routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import "./App.scss"
-import NavBar from './Components/NavBar'
-import HomePage from './Pages/HomePage'
+import "./App.scss";
+import NavBar from "./Components/NavBar";
+import HomePage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
+import PortfoliosPage from "./Pages/PortfoliosPage";
+import BlogsPage from "./Pages/BlogsPage";
+import ContactPage from "./Pages/ContactPage";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="sidebar">
-        <NavBar/>
+        <NavBar />
       </div>
-    <div className='main-content'>
-        <div className='content'>
-          <HomePage/>
+      <div className="main-content">
+        <div className="content">
+          <Switch>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+
+            <Route path="/about" exact>
+              <AboutPage />
+            </Route>
+
+            <Route path="/portfolios" exact>
+              <PortfoliosPage />
+            </Route>
+
+            <Route path="/blogs" exact>
+              <BlogsPage />
+            </Route>
+
+            <Route path="/contact" exact>
+              <ContactPage />
+            </Route>
+          </Switch>
         </div>
-    </div>
-      
+      </div>
     </div>
   );
 }
