ad60fa5502b050e43a56a329a8297f92dfebf788
juancedeno96 <cedenojj7@gmail.com>
2021-04-23 11:23:54 -0600

    new component

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React from "react";
+import about from "../img/about.jpg";
 
 function ImageSection() {
-    return (
-        <div className='ImageSection'>
-            <div className='img'>
-                <img src={about} alt=''/>
-            </div>
+  return (
+    <div className="ImageSection">
+      <div className="img">
+        <img src={about} alt="" />
+      </div>
+      <div className="about-info">
+        <h4>
+          I am<span> Juan Cedeno</span>
+        </h4>
+        <p className="about-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec
+          maximus arcu. Phasellus et elit elit. Maecenas sed sodales dolor,
+          placerat scelerisque eros.
+        </p>
+        <div className="about-details">
+          <div className="left-section">
+            <p>Full Name:</p>
+            <p>Age:</p>
+            <p>Nationality:</p>
+            <p>Languages:</p>
+          </div>
+          <div className="right-section">
+            <p> Juan Jose Cedeno</p>
+            <p> 25</p>
+            <p> Ecuador</p>
+            <p> Spanish, English</p>
+          </div>
         </div>
-    )
+        <button className='btn'>Download CV</button>
+
+      </div>
+    </div>
+  );
 }
 
-export default ImageSection
+export default ImageSection;
---
+++ src/Components/SkillsSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SkillsSection() {
+    return (
+        <div className='SkillsSection'>
+            
+        </div>
+    )
+}
+
+export default SkillsSection
