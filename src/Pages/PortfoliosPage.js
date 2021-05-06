d42b44f84a534ebb661e2b860fd36ae3a886822d
juancedeno96 <cedenojj7@gmail.com>
2021-05-06 11:33:53 -0600

    added MenuItems component

--- src/Pages/PortfoliosPage.js
+++ src/Pages/PortfoliosPage.js
@@ -1,14 +1,21 @@
 import React from "react";
 import Title from "../Components/Title";
+import Categories from "../Components/Categories";
+import MenuItems from "../Components/MenuItems";
+import portfolios from "../Components/allPortfolios";
+import { useState } from "react";
 
 const PortfoliosPage = () => {
+  const [categories, setCategories] = useState(null);
+  const [menuItems, setMenuItems] = useState(portfolios);
   return (
     <div className="PortfolioPage">
       <div p-title>
         <Title title={"Portfolios"} span={"Portfolios"} />
       </div>
-      <div className='portfolios'>
-
+      <div className="portfolios-data">
+        <Categories />
+        <MenuItems menuItem={menuItems} />
       </div>
     </div>
   );
